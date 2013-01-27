97d0d2d00a88887e9d1518f1a1219a5f4d790208
Andrew DeMaria <ademaria@mines.edu>
2013-01-27 11:43:48 -0700

    Seems to be working again.

--- project02/albums.rb
+++ project02/albums.rb
@@ -5,20 +5,22 @@ class HelloWorld
       request = Rack::Request.new( env )
       case request.path
       when "/form" then do_form( request )
-      when "/list" then do_form( request )
-      else do_bad_request
+      when "/list" then do_list( request )
+      else do_bad_request 
+      end
+
   end
 
   def do_form( request )
-
+      [ 200, {"Content-Type" => "text/plain" }, ["Do form" ]]
   end
 
   def do_list( request )
-
+      [ 200, {"Content-Type" => "text/plain" }, ["Do list" ]]
   end
 
-  def do_bad_request( request )
-
+  def do_bad_request( )
+      [ 404, {"Content-Type"=> "text/plain" }, ["Are you crazy?!"]]
   end
 
 end
