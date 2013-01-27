76862babdc6aeed2fc1607aa3460976a099d5384
Andrew DeMaria <ademaria@mines.edu>
2013-01-27 14:17:47 -0700

    A little refactoring in prep for list work

--- project02/albums.rb
+++ project02/albums.rb
@@ -1,35 +1,5 @@
 require 'rack'
-require 'erb'
-
-class Form
-    def initialize( formLocation )
-        @rank = Hash.new
-        @order = Hash.new
-        if File.readable?( formLocation) 
-            @rawForm = File.open( formLocation, "r" ) { |f| f.read }
-        else
-            @rawForm = "<html>Form Error!</html>"
-        end
-        @erb = ERB.new( @rawForm )
-    end
-
-    def newOrder( value, display )
-        @order[value] = display
-    end
-
-    def newRank( value, display )
-        @rank[value] = display
-    end
-
-    def render()
-        @erb.result( get_binding )
-    end
-
-    def get_binding()
-        binding()
-    end
-
-end
+require_relative 'form'
 
 class WebApp
   def call(env)
