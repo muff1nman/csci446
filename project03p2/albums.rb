e3fe7682be3e3c512897592842af6dac18f8fd65
Andrew DeMaria <ademaria@mines.edu>
2013-02-08 11:30:46 -0700

    albums now gives back form correctly

--- project03p2/albums.rb
+++ project03p2/albums.rb
@@ -6,6 +6,17 @@ DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/albums.sqlite3.db" )
 
 set :port, 8080
 
-get "/" do
-    "Sinatra is working!"
+before do
+    @rankValues = Hash.new
+    (1..100).each { |value| @rankValues[value] = value }
 end
+
+get "/form" do
+    erb :form, :locals => { :rank => @rankValues }
+end
+
+post "/list" do
+    "Sinatra is responding"
+end
+
+
