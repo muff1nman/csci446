a6e48e6a0e5caa1551673b03d36752c43558662e
Andrew DeMaria <ademaria@mines.edu>
2013-03-02 14:54:58 -0700

    Unit test for product title uniqueness

--- project04/test/unit/product_test.rb
+++ project04/test/unit/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+    fixtures :products
 
     test "product attributes must not be empty" do
         product = Product.new
@@ -48,5 +49,14 @@ class ProductTest < ActiveSupport::TestCase
         bad.each do |bad_url|
             assert new_product(bad_url).invalid?, "#{bad_url} shouldn't be valid"
         end
+    end
+
+    test "product is not valid without a unique title" do
+        product = Product.new(title: products(:ruby).title,
+                              description: "hello",
+                              price: 23,
+                              image_url: "fred.gif")
+        assert !product.save
+        assert_equal "has already been taken", product.errors[:title].join('; ')
     end
 end
