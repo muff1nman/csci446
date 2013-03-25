b65b7bdbdffe9271104a4934f4067880ffc3e9a5
Andrew DeMaria <ademaria@mines.edu>
2013-03-25 11:11:35 -0600

    Added session counter for visits to the store index

--- project04/app/controllers/store_controller.rb
+++ project04/app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   def index
+      session[:counter] = 0 if session[:counter].nil?
+      session[:counter] += 1
+
       @products = Product.order(:title)
   end
 end
