b1b212d1fb250f3a5157ea265069741f3704adfc
Andrew DeMaria <ademaria@mines.edu>
2013-03-25 11:26:48 -0600

    Added counter variable showing number of visits on store index view

--- project04/app/controllers/store_controller.rb
+++ project04/app/controllers/store_controller.rb
@@ -2,7 +2,7 @@ class StoreController < ApplicationController
   def index
       session[:counter] = 0 if session[:counter].nil?
       session[:counter] += 1
-
+      @counter = session[:counter]
       @products = Product.order(:title)
   end
 end
