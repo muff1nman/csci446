21819edf7658e2814d0618038fb9bad917104d95
Andrew DeMaria <ademaria@mines.edu>
2013-03-27 20:36:41 -0600

    Added admin links for the general layout

--- project04/app/models/user.rb
+++ project04/app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 5 }
   has_secure_password
   attr_accessible :name, :password, :password_confirmation
+
+  after_destroy :ensure_an_admin_remains
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end
+
 end
